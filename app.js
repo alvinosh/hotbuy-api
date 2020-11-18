52b3d28b4ce22eda862824449e4baee57562eb40
Alvi Hysa <alvihysa89@gmail.com>
2020-11-18 20:30:18 +0100

    Basic routing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const sequelize = require("./db/sequelize");
 require("dotenv").config({ path: "./config/.env" });
 
@@ -18,13 +20,29 @@ async function initServer() {
   await assertDatabaseConnection();
 
   const app = express();
+  app.use(bodyParser.json());
+
   const PORT = process.env.PORT;
 
-  console.log(`Starting server on port ${PORT}`);
+  console.log(`Starting server on port${PORT}`);
 
-  app.get("/api/posts", (req, res) => {
-    const users = sequelize.models.Post.findAll();
-    res.status(200).json(users);
+  app.post("/api/posts", (req, res) => {
+    sequelize.models.Post.create({
+      title: "lol",
+      description: "idek lmao"
+    }).then((post) => res.json(post));
+  });
+
+  app.get("/api/posts", async (req, res) => {
+    console.log(req.query.id);
+    const post = await sequelize.models.Post.findByPk(req.query.id);
+    if (post === null) {
+      console.log("Not found!");
+    } else {
+      console.log(post instanceof sequelize.models.Post); // true
+      // Its primary key is 123
+    }
+    res.status(200).json(post);
   });
 
   app.listen(PORT);
--- db/models/Post.js
+++ db/models/Post.js
@@ -1,22 +1,20 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Post",
-    {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
-      },
-      title: {
-        allowNull: false,
-        type: DataTypes.STRING
-      }
+  sequelize.define("Post", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
     },
-    {
-      tableName: "Posts"
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.STRING
     }
-  );
+  });
 };
