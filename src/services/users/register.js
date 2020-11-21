6fa73a7d61534f7ee94e69e7f6af937e2c667eca
Alvi Hysa <alvihysa89@gmail.com>
2020-11-21 20:34:27 +0100

    Hashed passwords

--- src/services/users/register.js
+++ src/services/users/register.js
@@ -1,9 +1,14 @@
+const bcrypt = require("bcryptjs");
+
 module.exports = async (data, db) => {
   const { User } = db.models;
 
   try {
     const emailExist = await User.findOne({ email: data.email });
     const usernameExist = await User.findOne({ username: data.username });
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(data.password, salt);
 
     if (emailExist) {
       throw new Error("Email is in use");
@@ -13,7 +18,14 @@ module.exports = async (data, db) => {
       throw new Error("Username is in use");
     }
 
-    return await User.create(data);
+    return await User.create({
+      username: data.username,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      email: data.email,
+      password: hash,
+      phone: data.phone
+    });
   } catch (error) {
     throw error;
   }
